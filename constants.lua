e88c47ed30771ada362585e58fc53f2f1acf7cae
Shuan Wang <shuanwang@gmail.com>
2016-10-31 00:45:05 -0700

    remove readonlytable prop

--- constants.lua
+++ constants.lua
@@ -1,36 +1,23 @@
-function readonlytable(table)
-   return setmetatable({}, {
-     __index = table,
-     __newindex = function(table, key, value)
-                    error("Attempt to modify read-only table")
-                  end,
-     __metatable = false
-   });
-end
-
-Direction = readonlytable {
+Direction = {
   RIGHT = 1,
   LEFT = 2
 }
 
--- BUTTON_A = 0x80
--- BUTTON_B = 0x40
-
-BUTTON = readonlytable {
+BUTTON = {
   A = 0x80,
   B = 0x40,
   UP = 0x80,
   DOWN = 0x40
 }
 
-POWERUP = readonlytable {
+POWERUP = {
   MUSHROOM = 0,
   FLOWER = 1,
   STAR = 2,
   ONEUP = 3
 }
 
-LEVEL_PALETTE = readonlytable {
+LEVEL_PALETTE = {
   NORMAL = 0,
   UNDERWATER = 1,
   NIGHT = 2,
