0cf20192e9fbd51c0adb178f4ca70bc56159a4a2
Shuan Wang <shuanwang@gmail.com>
2016-10-31 01:10:58 -0700

    re-add readonlytable into constants

--- constants.lua
+++ constants.lua
@@ -1,23 +1,33 @@
-Direction = {
+function readonlytable(table)
+   return setmetatable({}, {
+     __index = table,
+     __newindex = function(table, key, value)
+                    error("Attempt to modify read-only table")
+                  end,
+     __metatable = false
+   })
+end
+
+Direction = readonlytable {
   RIGHT = 1,
   LEFT = 2
 }
 
-BUTTON = {
+BUTTON = readonlytable {
   A = 0x80,
   B = 0x40,
   UP = 0x80,
   DOWN = 0x40
 }
 
-POWERUP = {
+POWERUP = readonlytable {
   MUSHROOM = 0,
   FLOWER = 1,
   STAR = 2,
   ONEUP = 3
 }
 
-LEVEL_PALETTE = {
+LEVEL_PALETTE = readonlytable {
   NORMAL = 0,
   UNDERWATER = 1,
   NIGHT = 2,
