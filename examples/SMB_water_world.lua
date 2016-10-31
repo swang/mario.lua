82121590a6304fc5b8e2f3d2e2cefd45a2cddf6a
Shuan Wang <shuanwang@gmail.com>
2016-10-31 02:13:40 -0700

    allow water world to be enabled/disabled via "Z"

--- examples/SMB_water_world.lua
+++ examples/SMB_water_world.lua
@@ -2,11 +2,26 @@ package.path =  "../?.lua;" .. package.path
 
 require "../world"
 
+local enable_swim_world = true
 
 World.set_level_palette(1)
+
 while (true) do
-  World.set_time(999)
-  World.set_bubbles(true)
-  World.enable_swimming()
+
+  local key = input.get()
+
+  if key["Z"] then
+    enable_swim_world = not enable_swim_world --> toggle swim world
+  end
+
+  if (enable_swim_world) then
+    World.set_time(999)
+    World.set_bubbles(true)
+    World.enable_swimming()
+  else
+    World.set_bubbles(false)
+    World.disable_swimming()
+  end
+
   FCEU.frameadvance()
 end
